a6611b4fa5b1d095a6222d4d89702325bb4432e4
root <root@LAPTOP-GMAHP08P.localdomain>
2022-10-16 20:45:50 +0530

    updated navbar and about

---
+++ Protfolio.js
@@ -0,0 +1,11 @@
+let showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
