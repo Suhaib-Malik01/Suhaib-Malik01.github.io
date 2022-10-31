0121a6b9f3b59cf60073bb640ea63f39f9169fdd
root <root@LAPTOP-GMAHP08P.localdomain>
2022-10-31 19:11:38 +0530

    Updated photo ,project, github Status

--- Protfolio.js
+++ Protfolio.js
@@ -1,11 +1,21 @@
-let showMenu = (toggleId, navId) =>{
+let showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
+
+
+window.onload = function () {
+    GitHubCalendar(".calendar", "Suhaib-Malik01");
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "Suhaib-Malik01", { responsive: true });
+    
+}
+
+
