316d8de1e964b0c5a5766a83ac0a897988374185
Suhaib Malik <suhaib.malik9691@gmail.com>
2022-11-07 22:01:08 +0530

    Updated portfolio

--- Protfolio.js
+++ Protfolio.js
@@ -17,3 +17,53 @@ window.onload = function () {
     GitHubCalendar(".calendar", "Suhaib-Malik01", { responsive: true });
 
 }
+
+let arr = [
+    {
+        "name": "Java",
+        "icon": "https://img.icons8.com/color/48/000000/java-coffee-cup-logo--v1.png"
+    },
+    {
+        "name": "MySQL",
+        "icon": "https://img.icons8.com/color/48/000000/mysql-logo.png"
+    },
+    {
+        "name": "Spring",
+        "icon": "https://img.icons8.com/color/48/000000/spring-logo.png"
+    },
+    {
+        "name": "HTML",
+        "icon": "https://img.icons8.com/color/48/000000/html-5--v1.png"
+    },
+    {
+        "name": "CSS3",
+        "icon": "https://img.icons8.com/color/48/000000/css3.png"
+    },
+    {
+        "name": "JavaScript",
+        "icon": "https://img.icons8.com/color/48/000000/javascript--v1.png"
+    },
+
+    {
+        "name": "GitHub",
+        "icon": "https://img.icons8.com/glyph-neue/48/github.png"
+    }
+]
+
+
+let container = document.getElementById("Skills");
+
+
+display(arr);
+function display(arr){
+    container.innerHTML = "";
+    arr.forEach(ele => {
+        let div = document.createElement("div");
+        let img = document.createElement("img");
+        img.src = ele.icon;
+        let name = document.createElement("h5");
+        name.innerText = ele.name;
+        div.append(img,name);
+        container.append(div);
+    });
+}
