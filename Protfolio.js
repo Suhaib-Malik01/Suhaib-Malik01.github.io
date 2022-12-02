5e1f3dd5e99c253eca70f10b3c377fc67f79be9a
Suhaib Malik <suhaib.malik9691@gmail.com>
2022-12-02 16:43:43 +0530

    added scroll animation

--- Protfolio.js
+++ Protfolio.js
@@ -14,8 +14,19 @@ let showMenu = (toggleId, navId) => {
 }
 showMenu('nav-toggle', 'nav-menu')
 
+const navlink = document.querySelectorAll('.nav-link')
 
 
+function linkAction(){
+    navlink.forEach(n=> n.classList.remove('active'))
+    this.classList.add('active')
+
+    let navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show')
+}
+
+navlink.forEach(n => n.addEventListener('click',linkAction))
 
 window.onload = function () {
     GitHubCalendar(".calendar", "Suhaib-Malik01");
